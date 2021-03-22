350a6b8c572fdd30941ba806704c279507fbded1
AvocadoDevops <github@mafilho.com. br>
2021-03-22 07:30:46 -0700

    Inclusão de label, textBox e botão para cálculo de IMC.

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -29,7 +29,85 @@ namespace SeuIMC
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.altura = new System.Windows.Forms.TextBox();
+            this.peso = new System.Windows.Forms.TextBox();
+            this.calcular = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(29, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Insira abaixo, sua altura";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(58, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(193, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Exemplo: 176 para 1m76cm";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.Location = new System.Drawing.Point(354, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(253, 32);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Insira abaixo, seu peso";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.Lime;
+            this.label4.Location = new System.Drawing.Point(405, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Exemplo: 80 para 80kg";
+            // 
+            // altura
+            // 
+            this.altura.Location = new System.Drawing.Point(101, 97);
+            this.altura.Name = "altura";
+            this.altura.Size = new System.Drawing.Size(100, 23);
+            this.altura.TabIndex = 5;
+            // 
+            // peso
+            // 
+            this.peso.Location = new System.Drawing.Point(431, 97);
+            this.peso.Name = "peso";
+            this.peso.Size = new System.Drawing.Size(100, 23);
+            this.peso.TabIndex = 6;
+            // 
+            // calcular
+            // 
+            this.calcular.Location = new System.Drawing.Point(281, 200);
+            this.calcular.Name = "calcular";
+            this.calcular.Size = new System.Drawing.Size(75, 23);
+            this.calcular.TabIndex = 7;
+            this.calcular.Text = "Calcular";
+            this.calcular.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
@@ -37,7 +115,14 @@ namespace SeuIMC
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlText;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(706, 383);
+            this.ClientSize = new System.Drawing.Size(635, 261);
+            this.Controls.Add(this.calcular);
+            this.Controls.Add(this.peso);
+            this.Controls.Add(this.altura);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -46,9 +131,18 @@ namespace SeuIMC
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cálculo IMC";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox altura;
+        private System.Windows.Forms.TextBox peso;
+        private System.Windows.Forms.Button calcular;
     }
 }
